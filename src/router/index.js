e4d120b59766427958e0d8904838b89c196ea601
r6rap <ahmadirafif7@gmail.com>
2025-07-20 02:27:22 +0700

    added several additional routers

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '../views/HomeView.vue'
 import CreatePost from '@/views/CreatePost.vue'
 import PostDetail from '@/views/PostDetail.vue'
 import EditPost from '@/views/EditPost.vue'
+import Register from '@/views/Register.vue'
+import Login from '@/views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,21 +15,44 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
       path: '/create/post',
       name: 'CreatePost',
-      component: CreatePost
+      component: CreatePost,
+      meta: { requiresAuth: true }
     },
     {
       path: '/detail/post/:id',
       name: 'PostDetail',
-      component: PostDetail
+      component: PostDetail,
+      meta: { requiresAuth: true }
     },
     {
       path: '/edit/post/:id',
       name: 'EditPost',
-      component: EditPost
-    }
+      component: EditPost,
+      meta: { requiresAuth: true }
+    },
   ],
+})
+
+// middleware check auth
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = JSON.parse(localStorage.getItem('isAuthenticated'))
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
